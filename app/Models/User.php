b2a6a75e53ee47dc2a5ef82c491fa5a53fb7d9e1
Boukhzare <programmerwadie@gmail.com>
2026-04-11 13:38:37 +0100

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = ['name', 'email', 'password'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
